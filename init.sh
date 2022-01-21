91b2846f7c82cb77f51c7d3f29581a061cb03593
ratneo <ratneo@pm.me>
2022-01-21 17:42:15 +0800

    add network related scripts

--- init.sh
+++ init.sh
@@ -9,19 +9,19 @@ BLUE="\033[36m"     # Info message
 PLAIN='\033[0m'
 
 coloredEcho() {
-    echo -e "${1}${@:2}${PLAIN}"
+  echo -e "${1}${@:2}${PLAIN}"
 }
 
 checkRoot() {
-    result=$(id | awk '{print $1}')
-    if [[ $result != "uid=0(root)" ]]; then
-        coloredEcho $RED " 请以root身份执行该脚本"
-        exit 1
-    fi
+  result=$(id | awk '{print $1}')
+  if [[ $result != "uid=0(root)" ]]; then
+    coloredEcho $RED " 请以root身份执行该脚本"
+    exit 1
+  fi
 }
 
 apt_source() {
-    cat > /etc/apt/sources.list<<-EOF
+  cat > /etc/apt/sources.list<<-EOF
 deb http://deb.debian.org/debian bullseye main
 deb-src http://deb.debian.org/debian bullseye main
 
@@ -34,17 +34,16 @@ deb-src http://deb.debian.org/debian bullseye-updates main
 deb http://deb.debian.org/debian bullseye-backports main
 deb-src http://deb.debian.org/debian bullseye-backports main
 EOF
-    apt update -y && apt upgrade -y
-    apt install curl wget git less screen xz-utils net-tools dnsutils mtr unzip iperf3 jq nethogs iftop lsof sudo certbot python3-certbot-nginx -y
-    coloredEcho $GREEN " 初始化完成"
+  apt update -y && apt upgrade -y
+  apt install curl wget git less screen xz-utils net-tools dnsutils mtr unzip iperf3 jq nethogs iftop lsof sudo certbot python3-certbot-nginx -y
+  coloredEcho $GREEN " 初始化完成"
 }
 
 fail2ban_install() {
-    
-    apt install fail2ban -y
-    systemctl enable --now fail2ban
+  apt install fail2ban -y
+  systemctl enable --now fail2ban
 
-    cat > /etc/fail2ban/jail.local <<-EOF
+  cat > /etc/fail2ban/jail.local <<-EOF
 [sshd]
 enabled   = true
 mode      = aggressive
@@ -54,33 +53,34 @@ findtime  = 48h
 maxretry  = 3
 EOF
 
-    fail2ban-client restart
-    coloredEcho $GREEN " Fail2Ban 安装完成"
+  fail2ban-client restart
+  coloredEcho $GREEN " Fail2Ban 安装完成"
 }
 
 ssh_key_install() {
-    wget --no-check-certificate https://raw.githubusercontent.com/wesleywxie/SSHKEY_Installer/master/key.sh
-    bash key.sh wesleywxie
+  wget --no-check-certificate https://raw.githubusercontent.com/wesleywxie/SSHKEY_Installer/master/key.sh
+  bash key.sh wesleywxie
 
-    sed -i "/#Port 22/c Port 30022" sshd_config
-    sed -i "/Port 22/c Port 30022" sshd_config
-    service sshd restart
-    service ssh restart
-    systemctl restart sshd
-    systemctl restart ssh
-    coloredEcho $GREEN " SSH Key 安装完成"
+  sed -i "/#Port 22/c Port 30022" sshd_config
+  sed -i "/Port 22/c Port 30022" sshd_config
+  service sshd restart
+  service ssh restart
+  systemctl restart sshd
+  systemctl restart ssh
+  
+  coloredEcho $GREEN " SSH Key 安装完成"
 }
 
 cloudflare_doh_install() {
-    bash <(curl -sL https://github.com/wikihost-opensource/centos-init/raw/main/network/dns-over-https/cloudflare.sh)
-    rm wikihost_cloudflare_doh_install.log
-    chattr +i /etc/resolv.conf
-    coloredEcho $GREEN " Cloudflare-DOH 安装完成"
+  bash <(curl -sL https://github.com/wikihost-opensource/centos-init/raw/main/network/dns-over-https/cloudflare.sh)
+  rm wikihost_cloudflare_doh_install.log
+  chattr +i /etc/resolv.conf
+  coloredEcho $GREEN " Cloudflare-DOH 安装完成"
 }
 
 besttrace_install() {
-    wget -O /usr/local/bin/besttrace "https://od.okra.cf/api?path=/tools/besttrace&raw=true" && chmod +x /usr/local/bin/besttrace
-    coloredEcho $GREEN " BestTrace 安装完成"
+  wget -O /usr/local/bin/besttrace "https://od.okra.cf/api?path=/tools/besttrace&raw=true" && chmod +x /usr/local/bin/besttrace
+  coloredEcho $GREEN " BestTrace 安装完成"
 }
 
 checkRoot
@@ -90,4 +90,9 @@ ssh_key_install
 cloudflare_doh_install
 besttrace_install
 
-coloredEcho $GREEN " 系统初始化完成，建议重启后安装BBR"
+read -p "系统初始化完成，建议重启后安装BBR，是否现在重启 ? [Y/n] :" yn
+[ -z "${yn}" ] && yn="y"
+if [[ $yn == [Yy] ]]; then
+  echo -e "${Info} VPS 重启中..."
+  reboot
+fi
---
+++ network.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+# Install BBR and BBR related configurations
+# Author: ratneo<https://ihost.wiki>
+
+RED="\033[31m"      # Error message
+GREEN="\033[32m"    # Success message
+YELLOW="\033[33m"   # Warning message
+BLUE="\033[36m"     # Info message
+PLAIN='\033[0m'
+
+coloredEcho() {
+  echo -e "${1}${@:2}${PLAIN}"
+}
+
+checkReady() {
+  result=$(id | awk '{print $1}')
+  if [[ $result != "uid=0(root)" ]]; then
+    coloredEcho $YELLOW " 请以root身份执行该脚本"
+    exit 1
+  fi
+
+  if [[ ! -d /etc/letsencrypt/live ]]; then
+    coloredEcho $YELLOW " 请确保证书已通过certbot自主申请完毕"
+    return
+  fi
+}
+
+remove_all_bbr_config() {
+  rm -rf /etc/sysctl.d/*.conf
+  #rm -rf /etc/sysctl.conf
+  #touch /etc/sysctl.conf
+  if [ ! -f "/etc/sysctl.conf" ]; then
+    touch /etc/sysctl.conf
+  else
+    cat /dev/null >/etc/sysctl.conf
+  fi
+  sysctl --system
+  sed -i '/DefaultTimeoutStartSec/d' /etc/systemd/system.conf
+  sed -i '/DefaultTimeoutStopSec/d' /etc/systemd/system.conf
+  sed -i '/DefaultRestartSec/d' /etc/systemd/system.conf
+  sed -i '/DefaultLimitCORE/d' /etc/systemd/system.conf
+  sed -i '/DefaultLimitNOFILE/d' /etc/systemd/system.conf
+  sed -i '/DefaultLimitNPROC/d' /etc/systemd/system.conf
+
+  sed -i '/soft nofile/d' /etc/security/limits.conf
+  sed -i '/hard nofile/d' /etc/security/limits.conf
+  sed -i '/soft nproc/d' /etc/security/limits.conf
+  sed -i '/hard nproc/d' /etc/security/limits.conf
+
+  sed -i '/ulimit -SHn/d' /etc/profile
+  sed -i '/ulimit -SHn/d' /etc/profile
+  sed -i '/required pam_limits.so/d' /etc/pam.d/common-session
+
+  systemctl daemon-reload
+
+  rm -rf bbrmod
+  sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
+  sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+  sed -i '/fs.file-max/d' /etc/sysctl.conf
+  sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+  sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+  sed -i '/net.core.rmem_default/d' /etc/sysctl.conf
+  sed -i '/net.core.wmem_default/d' /etc/sysctl.conf
+  sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+  sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_recycle/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+  sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+  sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+  sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+  if [[ -e /appex/bin/lotServer.sh ]]; then
+    bash <(wget -qO- https://git.io/lotServerInstall.sh) uninstall
+  fi
+  coloredEcho $GREEN " 清除加速完成。"
+  sleep 1s
+}
+
+#启用BBR+fq
+startbbrfq() {
+  remove_bbr_lotserver
+  echo "net.core.default_qdisc=fq" >>/etc/sysctl.d/99-sysctl.conf
+  echo "net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.d/99-sysctl.conf
+  sysctl --system
+  coloredEcho $GREEN " BBR+FQ修改成功"
+}
+
+#优化系统配置
+optimizing_system() {
+  if [ ! -f "/etc/sysctl.conf" ]; then
+    touch /etc/sysctl.conf
+  fi
+  sed -i '/net.ipv4.tcp_retries2/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf
+  sed -i '/fs.file-max/d' /etc/sysctl.conf
+  sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.route.gc_timeout/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_synack_retries/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_syn_retries/d' /etc/sysctl.conf
+  sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
+  sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_max_orphans/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.ip_forward/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_no_metrics_save/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_frto/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_rfc1337/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_sack/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_fack/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_window_scaling/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_adv_win_scale/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_moderate_rcvbuf/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf
+  sed -i '/net.core.rmem_max/d' /etc/sysctl.conf
+  sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
+  sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+  sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+
+  echo "net.ipv4.tcp_retries2 = 8
+net.ipv4.tcp_slow_start_after_idle = 0
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+# forward ipv4
+#net.ipv4.ip_forward = 1
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=1
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=2
+net.ipv4.tcp_moderate_rcvbuf=1
+net.ipv4.tcp_rmem=4096 65536 33554432
+net.ipv4.tcp_wmem=4096 65536 33554432 
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr" >>/etc/sysctl.conf
+sysctl -p && sysctl --system
+
+  echo "*               soft    nofile           1000000
+*               hard    nofile          1000000" >/etc/security/limits.conf
+  echo "ulimit -SHn 1000000" >>/etc/profile
+  coloredEcho $GREEN " 优化设置完成"
+}
+
+checkReady
+remove_all_bbr_config
+startbbrfq
+optimizing_system
+
+read -p "需要重启VPS后，才能生效系统优化配置，是否现在重启 ? [Y/n] :" yn
+[ -z "${yn}" ] && yn="y"
+if [[ $yn == [Yy] ]]; then
+  echo -e "${Info} VPS 重启中..."
+  reboot
+fi
