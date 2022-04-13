842a1db3198a8694ef575564abe80d8eb7f6f493
ratneo <98090136+ratneo@users.noreply.github.com>
2022-04-13 13:09:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -55,8 +55,21 @@ table ip fail2ban {
 }
 EOF
 
-echo "include \"/etc/nftables/fail2ban.conf\"" >> /etc/nftables.conf
-nft -f /etc/nftables/fail2ban.conf
+  echo "include \"/etc/nftables/fail2ban.conf\"" >> /etc/nftables.conf
+  nft -f /etc/nftables/fail2ban.conf
+
+  cat > /etc/fail2ban/action.d/nftables-common.local <<-EOF
+[Init]
+# Definition of the table used
+nftables_family = ip
+nftables_table  = fail2ban
+
+# Drop packets 
+blocktype       = drop
+
+# Remove nftables prefix. Set names are limited to 15 char so we want them all
+nftables_set_prefix =
+EOF
 
   cat > /etc/fail2ban/jail.local <<-EOF
 [sshd]
@@ -69,7 +82,12 @@ maxretry  = 3
 
 port    = 30022
 logpath = /var/log/auth.log
+
+banaction = nftables-multiport
+chain     = input
 EOF
+
+
 
   fail2ban-client restart
   coloredEcho $GREEN " Fail2Ban 安装完成"
