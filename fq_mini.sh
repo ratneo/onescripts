a9d788026a9466a428e01292675c4989f3885283
ratneo <98090136+ratneo@users.noreply.github.com>
2022-04-29 11:28:47 +0800

    Create fq_mini.sh

---
+++ fq_mini.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+# Install xray with configurations
+# Author: ratneo<https://ihost.wiki>
+
+RED="\033[31m"      # Error message
+GREEN="\033[32m"    # Success message
+YELLOW="\033[33m"   # Warning message
+BLUE="\033[36m"     # Info message
+PLAIN='\033[0m'
+
+XRAY_CONFIG_FILE="/usr/local/etc/xray/config.json"
+NGINX_CONF_PATH="/etc/nginx/conf.d/"
+NGINX_SERVICE_FILE="/lib/systemd/system/nginx.service"
+XRAY_VER="v1.5.4"
+
+coloredEcho() {
+  echo -e "${1}${@:2}${PLAIN}"
+}
+
+checkRoot() {
+  result=$(id | awk '{print $1}')
+  if [[ $result != "uid=0(root)" ]]; then
+    coloredEcho $YELLOW " 请以root身份执行该脚本"
+    exit 1
+  fi
+}
+
+archAffix(){
+    case "$(uname -m)" in
+        i686|i386)
+            echo '32'
+        ;;
+        x86_64|amd64)
+            echo '64'
+        ;;
+        armv5tel)
+            echo 'arm32-v5'
+        ;;
+        armv6l)
+            echo 'arm32-v6'
+        ;;
+        armv7|armv7l)
+            echo 'arm32-v7a'
+        ;;
+        armv8|aarch64)
+            echo 'arm64-v8a'
+        ;;
+        mips64le)
+            echo 'mips64le'
+        ;;
+        mips64)
+            echo 'mips64'
+        ;;
+        mipsle)
+            echo 'mips32le'
+        ;;
+        mips)
+            echo 'mips32'
+        ;;
+        ppc64le)
+            echo 'ppc64le'
+        ;;
+        ppc64)
+            echo 'ppc64'
+        ;;
+        ppc64le)
+            echo 'ppc64le'
+        ;;
+        riscv64)
+            echo 'riscv64'
+        ;;
+        s390x)
+            echo 's390x'
+        ;;
+        *)
+            coloredEcho $RED " 不支持的CPU架构！"
+            exit 1
+        ;;
+    esac
+
+	return 0
+}
+
+getInput() {
+  echo ""
+  read -p " 请设置连接密码（不输则随机生成）:" PASSWORD
+  [[ -z "$PASSWORD" ]] && PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
+  coloredEcho $BLUE " 密码：$PASSWORD"
+}
+
+installXray() {
+    rm -rf /tmp/xray
+    mkdir -p /tmp/xray
+    DOWNLOAD_LINK="https://github.com/XTLS/Xray-core/releases/download/${XRAY_VER}/Xray-linux-$(archAffix).zip"
+    coloredEcho $BLUE " 下载Xray: ${DOWNLOAD_LINK}"
+    curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip ${DOWNLOAD_LINK}
+    if [ $? != 0 ];then
+        coloredEcho $RED " 下载Xray文件失败，请检查服务器网络设置"
+        exit 1
+    fi
+    systemctl stop xray
+    mkdir -p /usr/local/etc/xray /usr/local/share/xray && \
+    unzip /tmp/xray/xray.zip -d /tmp/xray
+    cp /tmp/xray/xray /usr/local/bin
+    cp /tmp/xray/geo* /usr/local/share/xray
+    chmod +x /usr/local/bin/xray || {
+        coloredEcho $RED " Xray安装失败"
+        exit 1
+    }
+
+    cat >/etc/systemd/system/xray.service<<-EOF
+[Unit]
+Description=Xray Service
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+#User=nobody
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/xray run -config /usr/local/etc/xray/config.json
+Restart=on-failure
+RestartPreventExitStatus=23
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    systemctl daemon-reload
+    systemctl enable xray.service
+}
+
+
+configXray() {
+    mkdir -p /usr/local/xray
+   cat > $XRAY_CONFIG_FILE<<-EOF
+{
+  "log": {
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "port": 61481,
+      "protocol": "shadowsocks",
+      "settings": {
+        "clients": [
+          {
+            "method": "chacha20-poly1305",
+            "password": "$PASSWORD",
+            "level": 0
+          }
+        ],
+        "network": "tcp,udp"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "tag": "direct",
+      "protocol": "freedom",
+      "settings": {
+        "domainStrategy": "UseIPv4"
+      }
+    },
+    {
+      "tag":"IP6_out",
+      "protocol": "freedom",
+      "settings": {
+        "domainStrategy": "UseIPv6"
+      }
+    },
+    {
+      "tag": "blackhole",
+      "protocol": "blackhole",
+      "settings": {}
+    },
+    {
+      "tag": "proxy",
+      "protocol": "shadowsocks",
+      "settings": {
+        "servers": [
+          {
+            "address": "1.1.1.1",
+            "port": 61481,
+            "method": "chacha20-poly1305",
+            "password": "$PASSWORD"
+          }
+        ]
+      }
+    }
+  ],
+  "routing": {
+    "rules": [
+      {
+        "type": "field",
+        "protocol": [
+          "bittorrent"
+        ],
+        "outboundTag": "blackhole"
+      },
+      {
+        "type": "field",
+        "ip": [
+          "127.0.0.1/32",
+          "10.0.0.0/8",
+          "fc00::/7",
+          "fe80::/10",
+          "172.16.0.0/12"
+        ],
+        "outboundTag": "blackhole"
+      },
+      {
+        "type": "field",
+        "outboundTag": "direct",
+        "domain": [
+          "google.com",
+          "googlevideo.com",
+          "gstatic.com",
+          "youtube.com"
+        ]
+      },
+      {
+        "type": "field",
+        "outboundTag": "direct",
+        "domain": [
+          "geosite:netflix",
+          "amazonaws.com"
+        ]
+      },
+      {
+        "type": "field",
+        "outboundTag": "direct",
+        "network": "udp,tcp"
+      }
+    ]
+  }
+}
+EOF
+}
+
+install() {
+  apt clean all
+  apt update -y
+  apt install wget vim unzip tar gcc openssl net-tools libssl-dev g++ -y
+
+  coloredEcho $BLUE " 安装Xray ${XRAY_VER} ，架构$(archAffix)"
+  installXray
+  configXray
+
+  systemctl restart xray
+  sleep 2
+  coloredEcho $BLUE " 安装完成"
+}
+
+checkRoot
+getInput
+install
