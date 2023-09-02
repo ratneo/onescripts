048f768488f827d6211f6742aacc4574d2fdee77
ratneo <98090136+ratneo@users.noreply.github.com>
2023-09-02 20:49:03 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -9,17 +9,34 @@ BLUE="\033[36m"     # Info message
 PLAIN='\033[0m'
 
 HYSTERIA_CONF_PATH="/opt/hysteria"
-HYSTERIA_CONFIG_FILE="/opt/hysteria/config.json"
-HYSTERIA_VER="v1.3.5"
+HYSTERIA_CONFIG_FILE="/opt/hysteria/config.yaml"
+HYSTERIA_VER="v2.0.0"
 
 coloredEcho() {
   echo -e "${1}${@:2}${PLAIN}"
 }
 
+archAffix(){
+    case "$(uname -m)" in
+        x86_64|amd64)
+            echo 'amd64'
+        ;;
+        armv8|aarch64)
+            echo 'arm64'
+        ;;
+        *)
+            coloredEcho $RED " 不支持的CPU架构！"
+            exit 1
+        ;;
+    esac
+
+	return 0
+}
+
 installHysteria() {
     rm -rf ${HYSTERIA_CONF_PATH}
     systemctl stop hysteria
-    DOWNLOAD_LINK="https://github.com/HyNetwork/hysteria/releases/download/${HYSTERIA_VER}/hysteria-linux-amd64"
+    DOWNLOAD_LINK="https://github.com/apernet/hysteria/releases/download/${HYSTERIA_VER}/hysteria-linux-$(archAffix)"
     coloredEcho $BLUE " 下载Hysteria: ${DOWNLOAD_LINK}"
     curl -L -H "Cache-Control: no-cache" -o /usr/local/bin/hysteria ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
@@ -34,34 +51,53 @@ installHysteria() {
 
     mkdir -p ${HYSTERIA_CONF_PATH}
     cat > $HYSTERIA_CONFIG_FILE<<-EOF
-{
-  "listen": ":2083",
-  "cert": "/etc/letsencrypt/live/$TROJAN_DOMAIN/fullchain.pem",
-  "key": "/etc/letsencrypt/live/$TROJAN_DOMAIN/privkey.pem",
-  "obfs": "$PASSWORD",
-  "alpn": "h3",
-  "up_mbps": 500,
-  "down_mbps": 500,
-  "recv_window_conn": 15728640,
-  "recv_window_client": 67108864,
-  "max_conn_client": 4096,
-  "resolver": "udp://127.0.0.1:53",
-  "resolve_preference": "4"
+listen: :2083
+
+tls:
+  cert: /etc/letsencrypt/live/$TROJAN_DOMAIN/fullchain.pem
+  key: /etc/letsencrypt/live/$TROJAN_DOMAIN/privkey.pem
+
+auth:
+  type: password
+  password: $PASSWORD
+
+masquerade:
+  type: proxy
+  proxy:
+    url: https://bing.gifposter.com/
+    rewriteHost: true
+resolver:
+  type: udp
+  tcp:
+    addr: 127.0.0.1:53 
+    timeout: 4s 
+  udp:
+    addr: 127.0.0.1:53 
+    timeout: 4s
+outbounds:
+  - name: outbound
+    type: socks5
+    socks5:
+      addr: 127.0.0.1:7890 
 }
 EOF
 
 
     cat >/etc/systemd/system/hysteria.service<<-EOF
 [Unit]
-Description=Hysteria is a feature-packed proxy & relay utility powered by a customized QUIC protocol.
-Documentation=https://github.com/HyNetwork/hysteria/wiki
-After=network.target nss-lookup.target
+Description=Hysteria Server Service
+After=network.target
+
 [Service]
+Type=simple
+ExecStart=/usr/local/bin/hysteria server --config $HYSTERIA_CONFIG_FILE
+WorkingDirectory=$HYSTERIA_CONF_PATH
 User=root
+Environment=HYSTERIA_LOG_LEVEL=error
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW
 NoNewPrivileges=true
-ExecStart=/usr/local/bin/hysteria -c /opt/hysteria/config.json server
-Restart=on-failure
-RestartPreventExitStatus=23
+
 [Install]
 WantedBy=multi-user.target
 EOF
